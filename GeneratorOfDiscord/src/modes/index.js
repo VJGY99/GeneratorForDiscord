7cda8c85e5f97294d786b8541fe2ab5559c30e2c
VJGY99 <80971475+VJGY99@users.noreply.github.com>
2021-09-28 19:36:18 +0200

    Update index.js

--- GeneratorOfDiscord/src/modes/index.js
+++ GeneratorOfDiscord/src/modes/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-// Gather everything from current dir and export it
+// Gather everything from current 
 fs.readdirSync(__dirname).filter(file => file !== 'index.js').map(file => {
    module.exports[file.replace('.js', '')] = require(`${__dirname}/${file}`);
 });
